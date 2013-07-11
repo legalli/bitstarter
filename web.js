a0727f9e32a61fde44da11fb0fe812ea9c257537
Leandro Galli <legalli@gmail.com>
2013-07-11 21:39:20 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,6 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var indexFile = fs.readFileSync("./index.html");
   response.send(indexFile.toString);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
